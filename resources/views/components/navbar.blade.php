5b36cef5a1f0bca636683dfadf0d05114d0809ee
Vincent <23_vincenttheodores@student.smkti.net>
2025-08-22 11:12:36 +0800

    h

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -52,9 +52,9 @@
                 <div class="sb-sidenav-menu">
                     <div class="nav">
                         <div class="sb-sidenav-menu-heading">Core</div>
-                        <a class="nav-link" href="index.html">
+                        <a class="nav-link">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                            Dashboard
+                            Kiosk
                         </a>
                         <div class="sb-sidenav-menu-heading">Interface</div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"

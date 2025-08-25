93b58503698a5311a1fb09188fbd5e5af7aa60f4
NaufalGalang <galangpelo123@gmail.com>
2025-08-25 13:16:23 +0800

    runningtext

--- resources/views/admin/running_text/index_text.blade.php
+++ resources/views/admin/running_text/index_text.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.body')
-@section('title', 'running_text')
+@section('title', 'running text')
 @section('main')
 
- <main>
-        <div class="container-fluid px-4">
-            <h1 class="mt-4"></h1>
-            <ol class="breadcrumb mb-4">
-                <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
-            {{-- card --}}
-            {{--        <x-card-parent></x-card-parent> --}}
-            {{-- ============= --}}
-            {{-- chart --}}
-            {{--        <x-chart-sb></x-chart-sb> --}}
-            {{-- ============ --}}
-            {{-- data table --}}
-                        <x-table></x-table>
-            {{-- =========== --}}
-        </div>
-    </main>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Running Text</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Running Text</li>
+        </ol>
+
+        {{-- Running text tampil --}}
+       @if($data)
+    <div class="alert alert-secondary text-start">
+        {{ $data }}
+    </div>
+@endif
+
+        {{-- Panggil component (popup modal) --}}
+        <x-running-text-form :data="$data" />
+
+    </div>
+</main>
 @endsection

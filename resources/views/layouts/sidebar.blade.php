da21a8efdf7bf79416140a08532ffe8c8356e872
RPQ35 <alfajriropiq@gmail.com>
2025-08-25 09:57:14 +0800

    merge conflig

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -17,36 +17,37 @@
 
                 <a class="nav-link" href="/admin/account">
                     <div class="sb-nav-link-icon">
-                        @if (Request::is('admin/account'))
-                            <i class="fas fa-tachometer-alt" style=" color: cornflowerblue;"></i>
+                        @if (Request::is('admin/account') || Request::is('admin/account/create'))
+                            <i class="fa-solid fa-user"style=" color: cornflowerblue;"></i>
                         @else
-                            <i class="fas fa-tachometer-alt"></i>
+                            <i class="fa-solid fa-user"></i>
                         @endif
                     </div>
                     account
                 </a>
 
 
+                <a class="nav-link" href="/admin/locket">
+                    <div class="sb-nav-link-icon">
+                        @if (Request::is('admin/locket') || Request::is('admin/locket/create'))
+                            <i class="fa-solid fa-computer" style=" color: cornflowerblue;"></i>
+                        @else
+                            <i class="fa-solid fa-computer"></i>
+                        @endif
+                    </div>
+                    Locket
+                </a>
+
 
 
                 {{-- ============================== --}}
                 {{-- |  sidebar dengan dropdown   | --}}
                 {{-- ============================== --}}
-
                 {{-- <div class="sb-sidenav-menu-heading">Interface</div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                    data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                    Layouts
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
-                    data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                    </nav>
-                </div> --}}
+                <x-sidebar-with-dropdown>
+                    <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                    <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                </x-sidebar-with-dropdown> --}}
 
 
 

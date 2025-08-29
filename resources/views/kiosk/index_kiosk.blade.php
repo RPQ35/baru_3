4d54acfafe19b7bb2cec844da212d57731994058
Vincent <23_vincenttheodores@student.smkti.net>
2025-08-29 09:51:13 +0800

    services

---
+++ resources/views/kiosk/index_kiosk.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Tombol Oval</title>
+  <style>
+    .oval-button {
+
+      padding: 15px 30px;
+      background-color: #2d3e50;
+      color: white;
+      font-weight: bold;
+      border-radius: 50px;
+      border: 2px solid #999;
+      box-shadow: 2px 2px 5px rgba(0,0,0,0.3);
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin:  auto;
+      width: max-content;
+
+    }
+    body{
+        height: 97.8vh;
+
+    }
+
+  </style>
+</head>
+<body style="background: linear-gradient( #0096FF 0%, 	#89CFF0 25%, #F0FFFF 100%);)">
+  <div class="oval-button">
+    PPID PEJABAT PENGELOLA <br>
+    INFORMASI & DOKUMENTASI
+  </div>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -67,16 +67,11 @@ Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
             /** output var list
              *   index => $data
              */
-        Route::get('/services', 'index')->name('services.index');
-        Route::get('/services/create', 'create')->name('services.create');
-        Route::post('/services/store', 'store')->name('services.store');
-<<<<<<< HEAD
-        Route::post('/services/update/{id}', 'update')->name('services.update');
-=======
-        Route::post('/services/update', 'update')->name('services.update');
->>>>>>> 07944fd7427dd10a74878f8221c6e079cc954f6b
-        Route::delete('/services/{id}', 'destroy')->name('services.destroy');
-
+            Route::get('/services', 'index')->name('services.index');
+            Route::get('/services/create', 'create')->name('services.create');
+            Route::post('/services/store', 'store')->name('services.store');
+            Route::post('/services/update', 'update')->name('services.update');
+            Route::delete('/services/{id}', 'destroy')->name('services.destroy');
         });
         Route::controller(RunningTextController::class)->group(function () {
             /** output var list
